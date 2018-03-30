9e5a4366e087ef8cb8c5d0a9f16d0f593fca37eb
Jonathan Day <jday.day03@gmail.com>
2018-03-29 21:52:03 -0700

    Completed project menu page

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,4 +1,40 @@
 class ProjectController < ApplicationController
-  def view
+def new
+
+end
+
+def show
+  @project = Project.find(params[:id])
+
+
+  #@project = Project.find(params[:id])
+  puts @project.Title
+end
+def showAll
+    @projects = Project.all
+end
+
+def create
+    @project = Project.new(proj_params)
+   # newProject.Title=params[:project][:title]
+    #puts (params[:title])
+    #newProject.Description=params[:project][:description]
+    @project.save
+    redirect_to action: "showAll"
+  end
+
+def edit
+  
+
+end
+
+
+
+
+  private
+  def proj_params
+    params.require(:project).permit(:Title, :Description)
   end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'project/view'
-
+  get 'project/new'
+  post 'project/new'
+  get 'project/showAll'
+  get 'project/show/:id' => 'project#show'
+  resources :project
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
